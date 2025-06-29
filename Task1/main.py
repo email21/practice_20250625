8a2ab7956da2aeab13f415aeefe85894f523a5f9
email21 <steady.challenger@gmail.com>
2025-06-29 15:27:56 +0900

    Mission #1

--- Task1/main.py
+++ Task1/main.py
@@ -1,14 +1,24 @@
-from module1 import TextAnalyzer
-
+from module1 import NewTextAnalyzer
 def main():
     # 테스트용 파일 생성
     with open('test.txt', 'w', encoding='utf-8') as f:
         f.write("hello world hello python world hello")
 
     # 분석기 객체 생성 및 결과 출력
-    analyzer = TextAnalyzer('test.txt')
+    analyzer = NewTextAnalyzer('test.txt')
     print("총 단어 수:", analyzer.word_count())
-    print("가장 많이 나온 단어:", analyzer.most_common_word())
+    
+    word, count = analyzer.most_common_word()  # 가장 많이 나온 단어와 개수
+    print(f"가장 많이 나온 단어는 '{word}'이고, 개수는 {count}번입니다.")
+      
+    # 미션1- 2. 특정 단어 카운팅하기
+    with open('test.txt', 'r', encoding='utf-8') as f:
+        content = f.read()
+        print(content)
+    
+    print("위의 문장에서 갯수를 알고 싶은 단어를 입력하세요.")
+    new_word = input()
+    print(f"{new_word} 단어 수:", analyzer. target_word_count(new_word))
 
 if __name__ == "__main__":
     main()
--- Task1/module1.py
+++ Task1/module1.py
@@ -4,10 +4,16 @@ class TextAnalyzer:
         with open(filepath, 'r', encoding='utf-8') as f:
             self.text = f.read()
 
-    def word_count(self): # 글자수?
+    def word_count(self): # 총 단어 수
         return len(self.text.split())
 
     def most_common_word(self): # 가장 많이 나온 단어
         from collections import Counter
         words = self.text.split()
         return Counter(words).most_common(1)[0]
+    
+## 1. class 수정하기 - 함수 추가하기
+# TextAnalyzer 클래스 상속받아서 단어 숫자세는 함수 추가하기
+class NewTextAnalyzer(TextAnalyzer):
+    def target_word_count(self,target_word):
+        return self.text.split().count(target_word)
